2c07688e23e567afacd530ab8b763080884c4429
gadicus93 <gadicus@gmail.com>
2022-04-24 13:42:54 -0500

    Update serializers.py

--- portfolio/serializers.py
+++ portfolio/serializers.py
@@ -6,20 +6,21 @@ from rest_framework.validators import UniqueValidator
 
 
 class CustomerSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Customer
-
-        fields = ('pk', 'name', 'address','cust_number', 'city', 'state', 'zipcode', 'email', 'cell_phone')
+        fields = ('pk','name', 'address', 'cust_number', 'city', 'state', 'zipcode', 'email', 'cell_phone')
 
 
 class InvestmentSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Investment
-        fields = (
-        'pk', 'customer', 'category', 'description','cust_number', 'acquired_value', 'acquired_date', 'recent_value', 'recent_date')
+        fields = ('pk','customer', 'cust_number', 'category', 'description', 'acquired_value', 'acquired_date', 'recent_value', 'recent_date')
 
 
 class StockSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Stock
         fields = ('pk', 'customer', 'cust_number', 'symbol', 'name', 'shares', 'purchase_price', 'purchase_date')
@@ -50,17 +51,15 @@ class RegisterSerializer(serializers.ModelSerializer):
 
         return attrs
 
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name']
+        )
 
-def create(self, validated_data):
-    user = User.objects.create(
-        username=validated_data['username'],
-        email=validated_data['email'],
-        first_name=validated_data['first_name'],
-        last_name=validated_data['last_name']
-    )
+        user.set_password(validated_data['password'])
+        user.save()
 
-
-    user.set_password(validated_data['password'])
-    user.save()
-
-    return user
+        return user
